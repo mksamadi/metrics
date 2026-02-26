acf4dd642a70849acea6afccb612b800c8487a25
mksamadi <mksamadi@gmail.com>
2026-02-26 14:59:28 -0500

    Update: 2026-02-26 14:59:27

---
+++ sw.js
@@ -0,0 +1,42 @@
+﻿const CACHE_NAME = 'financial-analytics-v' + Date.now();
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/math.html',
+  '/probability.html',
+  '/econometrics.html',
+  '/simpleregression.html',
+  '/multipleregression.html',
+  '/qualitative.html',
+  '/introtime.html',
+  '/timeseries.html',
+  '/datawrangling.html',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
